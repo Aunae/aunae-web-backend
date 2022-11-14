bd5cb9f9c8358812b9a5a4b675af458a3d5a46f7
yoonki1207 <yoonki1207@naver.com>
2022-11-14 23:17:56 +0900

    Modefied board.entities.ts

--- src/users/entities/board.entities.ts
+++ src/users/entities/board.entities.ts
@@ -1,42 +1,41 @@
 import {
-    Column,
-    CreateDateColumn,
-    DeleteDateColumn,
-    Entity,
-    PrimaryGeneratedColumn,
-    ManyToOne,
-    Unique,
-    UpdateDateColumn,
-  } from 'typeorm';
-  import {User} from './user.entities'
-  @Entity()
-  
-  export class Board {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-    
-    @ManyToOne(()=>User,(user)=>user.boards)
-    author: string;
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entities';
 
-    @Column()
-    title: string;
-  
-    @Column()
-    thumnail: string;
-  
-    @Column()
-    description: string;
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    viewCount: Number;
-  
-    @CreateDateColumn()
-    createdAt: Date;
-  
-    @UpdateDateColumn()
-    updatedAt: Date;
-  
-    @DeleteDateColumn()
-    deletedAt: Date;
-  }
-  
+  @ManyToOne(() => User, (user) => user.boards)
+  author: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  thumnail: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  viewCount: Number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
