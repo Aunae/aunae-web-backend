874103ddf3d75ed81b57e9267e46fd907d7678df
yoonki1207 <yoonki1207@github.com>
2022-11-17 14:43:20 +0900

    Imported required.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { DatabaseModule } from './configs/database/database.module';
+import { AppConfigModule } from './configs/app/app.config.module';
 import { UserAct } from './user/entities/userAct.entities';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
@@ -6,12 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    AppConfigModule,
-    DatabaseModule,
-    AuthModule,
-    UserModule,
-  ],
+  imports: [AppConfigModule, DatabaseModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/database/connection.service.ts
+++ src/configs/database/connection.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/user/entities/user.entities';
+import { UserAct } from 'src/user/entities/userAct.entities';
+import { Board } from 'src/board/entities/board.entities';
+import { Comment } from 'src/comment/entities/comment.entities';
 
 @Injectable()
 export class ConnectionService implements TypeOrmOptionsFactory {
@@ -15,7 +19,7 @@ export class ConnectionService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_NAME'),
       autoLoadEntities: true,
-      entities: [],
+      entities: [User, UserAct, Board, Comment],
 
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
