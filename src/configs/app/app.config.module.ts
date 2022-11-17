2110a836d89e9e23f1c3fcf321d90332d00f1fd0
yoonki1207 <yoonki1207@github.com>
2022-11-17 14:51:47 +0900

    File name fixed.

--- src/configs/app/app.config.module.ts
+++ src/configs/app/app.config.module.ts
@@ -6,7 +6,7 @@ import * as Joi from 'joi';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
