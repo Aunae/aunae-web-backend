56765d0e7e1872f58a0771fbe1f02c8959d24b01
yoonki1207 <yoonki1207@github.com>
2022-11-18 15:02:31 +0900

    Added module

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,3 +1,4 @@
+import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentController } from './comment.controller';
@@ -5,7 +6,7 @@ import { CommentService } from './comment.service';
 import { Comment } from './entities/comment.entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment]), UserModule],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [TypeOrmModule],
