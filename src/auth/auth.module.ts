9b40e4f00f67285b146c7fb73a158f7d942c72c7
KGESH <seonghyeon.jee@gmail.com>
2022-11-24 16:43:38 +0900

    Style: cleanup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entities';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/constants';
