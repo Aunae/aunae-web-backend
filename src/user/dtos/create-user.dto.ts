862ee74cb6186bb987eca01873fa233eb6bf0d1e
KGESH <seonghyeon.jee@gmail.com>
2022-11-24 16:44:22 +0900

    Style: cleanup

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,9 +1,10 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { User } from '../entities/user.entities';
+import { IsString } from 'class-validator';
 
 // password의 class-validator는 user entity로 이동
-export class CreateUserDto extends PickType(User, [
-  'email',
-  'password',
-  'username',
-]) {}
+export class CreateUserDto extends PickType(User, ['email', 'username']) {
+  @ApiProperty()
+  @IsString()
+  password: string;
+}
