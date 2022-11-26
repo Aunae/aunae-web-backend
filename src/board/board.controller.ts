64cf822b556df2587bab8ce92a624db89e2d31e2
yoonki1207 <yoonki1207@github.com>
2022-11-26 15:02:10 +0900

    Feat: board controller, service

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private boardService: BoardService) {}
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string) {}
+
+  /**
+   * @Todo 어떤 "게시판"에 작성할 것인지를 명시하는 url이 필요할 것. 나중에 수정.
+   * @Todo 예를 들어 /board/notice면 공지사항, /board/community이면 자유게시판.. 등등
+   */
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  createBoard() {}
+}
