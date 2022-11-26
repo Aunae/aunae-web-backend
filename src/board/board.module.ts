1740890652bc9be4468ed7c04c53628ab1f5e423
yoonki1207 <yoonki1207@github.com>
2022-11-26 14:50:59 +0900

    Feat: added board service

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entities';
 import { UserModule } from '../user/user.module';
+import { BoardService } from './board.service';
 
 @Module({
   imports: [UserModule, TypeOrmModule.forFeature([Board])],
   exports: [TypeOrmModule],
+  providers: [BoardService],
 })
 export class BoardModule {}
