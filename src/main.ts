4abb9323942a6da6df56b10457738b6a2c187e95
yoonki1207 <yoonki1207@github.com>
2022-11-26 23:44:17 +0900

    Feat: set global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/swagger';
@@ -5,6 +6,11 @@ import { setupSwagger } from './util/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
